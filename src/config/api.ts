2ec1f31b5d5fe69d6c3651d05c4e0335501e5c57
Kiran Prasad Neupane <94504333+neupanekiran@users.noreply.github.com>
2024-12-26 00:28:26 +0545

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import { defaultPrompt } from '../prompts/danfetea';
 const API_CONFIG = {
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY || '',
+  apiKey: "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"
   apiUrl: 'https://api.openai.com/v1/chat/completions',
   model: 'gpt-3.5-turbo',
   defaultPrompt: defaultPrompt , 
