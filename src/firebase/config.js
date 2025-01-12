fb37e47a185f59f6fe303c1682d3347063a8296f
neupanekiran <94504333+neupanekiran@users.noreply.github.com>
2025-01-12 22:24:36 +0530

    firebase updates

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCxkraTOo3ZvAtwyKbOpIciNNBMmyHirI4",
+  authDomain: "chatbot-danfetea.firebaseapp.com",
+  projectId: "chatbot-danfetea",
+  storageBucket: "chatbot-danfetea.firebasestorage.app",
+  messagingSenderId: "107316525939",
+  appId: "1:107316525939:web:7fb9f24d681bcfd075a306",
+  measurementId: "G-XSH138VP9J"
 };
 
 // Initialize Firebase
